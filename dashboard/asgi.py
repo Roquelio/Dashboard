f61f6293879b9cc6da7ed5d46426f170b9178a08
Roquelio <androroques03@gmail.com>
2023-11-06 18:35:16 -0500

    test_dashboard

--- dashboard/asgi.py
+++ dashboard/asgi.py
@@ -9,8 +9,4 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 
 import os
 
-from django.core.asgi import get_asgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dashboard.settings')
-
-application = get_asgi_application()
