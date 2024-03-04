a81c16381af2d9cb7751b5321a720057d2e2721a
Roquelio <androroques03@gmail.com>
2024-03-04 15:17:40 -0500

    test_tables_scrap

--- task/static/js/scrap.js
+++ task/static/js/scrap.js
@@ -3,12 +3,34 @@ const listData = async () => {
         const response = await fetch("https://test-gliv.onrender.com/ver_datos");
         const data = await response.json();
 
-        ['usdt', 'btc', 'eth', 'bnb', 'dai', 'fdusd', 'doge', 'ada', 'xrp'].forEach((symbol) => {
-            ['buy', 'sell'].forEach((tradeType) => {
-                const symbolData = data[symbol];
+        ['buy2', 'sell2'].forEach((tradeType) => {
+            ['USDT', 'DAI', 'FDUSD'].forEach((symbol) => {
+                const symbolData = data[tradeType][symbol];
 
                 if (symbolData) {
-                    symbolData[tradeType]?.forEach((ad, index) => {
+                    symbolData.forEach((ad, index) => {
+                        updateTradeTypeData(data, symbol, tradeType, index);
+                    });
+                } else {
+                    console.error(`Datos no válidos recibidos para ${symbol}:`, data);
+                }
+            });
+        });
+    } catch (ex) {
+        console.error(ex);
+    }
+};
+const listData2 = async () => {
+    try {
+        const response = await fetch("https://test-gliv.onrender.com/ver_datos");
+        const data = await response.json();
+
+        ['buy', 'sell'].forEach((tradeType) => {
+            ['BTC', 'ETH', 'BNB', 'DOGE', 'ADA', 'XRP'].forEach((symbol) => {
+                const symbolData = data[tradeType][symbol];
+
+                if (symbolData) {
+                    symbolData.forEach((ad, index) => {
                         updateTradeTypeData(data, symbol, tradeType, index);
                     });
                 } else {
@@ -23,14 +45,14 @@ const listData = async () => {
 
 const updateTradeTypeData = (data, symbol, tradeType, index) => {
     const cell = $(`#${tradeType}-${symbol.toLowerCase()}-${index + 1}`);
-    const ad = data[symbol]?.[tradeType]?.[index];
+    const ad = data[tradeType][symbol][index];
 
     if (!ad) {
         console.error(`Datos no válidos recibidos para ${tradeType} de ${symbol}:`, data);
         return;
     }
 
-    cell.text(ad.TradeType === tradeType ? 'X' : '-');
+    cell.text('X'); // Marcar como 'X' ya que se recibió el dato
 
     const formattedInfo = `
     ${ad.NickName} \n
@@ -44,5 +66,7 @@ const updateTradeTypeData = (data, symbol, tradeType, index) => {
 };
 
 listData();
+listData2();
 
-setInterval(listData, 2000);
+setInterval(listData, 1000);
+setInterval(listData2, 1000);
